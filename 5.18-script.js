b043d4bd2444a6df1e7248487ad7d6d7001f0919
Fernando-org-oss <fernandoquevedo262008@gmail.com>
2025-06-25 16:08:25 -0300

    Add files via upload

---
+++ 5.18-script.js
@@ -0,0 +1,12 @@
+// Ler a distância da próxima corrida e dizer quanto tempo vai levar.
+// 1 dado de Entrada: Distância (em quilômetros);
+// 1 dado de Saída: Tempo de viagem (em segundos).
+// 
+let distancia = Number (prompt( "digite a distancia em Km:  " ))
+let tempo
+let velocidade = 300000
+
+     tempo = distancia  / velocidade
+     
+     alert (' A viagem vai durar ' + tempo + ' segundos!');
+    
